9d503b1924270369a760b110af6f955a3460a5de
AayshaS <aaysha.siddika@swqa-solutions.com>
2022-08-22 11:56:03 +0530

    New BH Project on Monday 22-08-2022

---
+++ src/test/java/tests/CheckNotificationOfSupportGroup_Test.java
@@ -0,0 +1,42 @@
+package tests;
+
+import common.BasePage;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import page_objects.CheckNotificationOfSupportGroup_PageObjects;
+import page_objects.LoginCheckBH_PageObjects;
+
+import java.io.IOException;
+import java.lang.reflect.Method;
+
+import static utils.extentreports.ExtentTestManager.startTest;
+
+public class CheckNotificationOfSupportGroup_Test extends BasePage {
+    LoginCheckBH_PageObjects loginChk;
+    CheckNotificationOfSupportGroup_PageObjects notificationChk;
+
+    @BeforeMethod
+    public void setUp() throws IOException {
+        initialization();
+        //loginChk =new LoginCheckBH_PageObjects();
+        notificationChk = new CheckNotificationOfSupportGroup_PageObjects();
+    }
+    @Test(priority = 1)
+    public void checkPermissionToggleOff(Method method) {
+        startTest(method.getName(), "This test is to check Toggle option.");
+        notificationChk.clickOnFirstLogIn();
+        notificationChk.provideValidLoginCredentials();
+        notificationChk.clickOnResources();
+        notificationChk.clickOnpermissonAndSetting();
+        notificationChk.clickOnResources();
+        notificationChk.clickOnLogOut();
+
+    }
+
+    @AfterMethod
+    public void tearDown()
+    {
+        closeDriver();
+    }
+}
